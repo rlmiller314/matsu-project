bea9f7077c4e1fbbb3b24e0ebffd97e241b0b4d9
Jim Pivarski <pivarski@colorglass-xubuntu1204.(none)>
2012-07-21 01:30:49 -0500

    Selected six bands for maximum contrast between water, wake, and cloud.

--- Analytics/Water and Boats/water_spectrum.py
+++ Analytics/Water and Boats/water_spectrum.py
@@ -1,5 +1,7 @@
 from math import *
 import glob
+import random
+import time
 
 import numpy
 from PIL import Image
@@ -151,3 +153,98 @@ cloudsellipse2 = Scatter(
 legend = Legend([[cloudsplot, "clouds"], [wakeplot, "wake of ship"], [waterplot, "water"], [waterellipse, "PCA analysis"], [waterellipse2, "PCA 2 sigma"]], colwid=[0.3, 0.7], justify="cl", width=0.37)
 
 view(Overlay(waterplot, wakeplot, cloudsplot, waterellipse, wakeellipse, cloudsellipse, waterellipse2, wakeellipse2, cloudsellipse2, legend, xlabel="band 16 radiance [W/m/m/sr/micron]", ylabel="band 150 radiance [W/m/m/sr/micron]", xmin=0., xmax=300., ymin=0., ymax=35.), fileName="plots_justtwobands.svg")
+
+#####################################################################################
+
+water = numpy.zeros(242, dtype=numpy.float)
+
+waters = []
+for pixelNumber in xrange(1000):
+    waters.append(numpy.zeros(242, dtype=numpy.float))
+
+wakes = []
+for pixelNumber in xrange(numpy.count_nonzero(masks["wake-tight"])):
+    wakes.append(numpy.zeros(242, dtype=numpy.float))
+
+clouds = []
+for pixelNumber in xrange(numpy.count_nonzero(masks["clouds-tight"])):
+    clouds.append(numpy.zeros(242, dtype=numpy.float))
+
+bands = []
+for bandNumber in xrange(1, 242+1):
+    data = glob.glob(inputDir + "/*_B%03d_L1T.TIF" % bandNumber)
+    if len(data) == 0: continue
+    data = gdal.Open(data[0], gdalconst.GA_ReadOnly)
+    if bandNumber < 70.5:
+        data = data.GetRasterBand(1).ReadAsArray() / 40.
+    else:
+        data = data.GetRasterBand(1).ReadAsArray() / 80.
+    data = data[2200:2800,197:358]
+    if data.mean() < 0.1: continue
+
+    print bandNumber
+    water[len(bands)] = log(data[masks["pure-water"]].mean())
+    for w, v in zip(waters, data[masks["pure-water"]][:1000]):
+        w[len(bands)] = log(v + 1e-10)
+    for w, v in zip(wakes, data[masks["wake-tight"]]):
+        w[len(bands)] = log(v + 1e-10)
+    for w, v in zip(clouds, data[masks["clouds-tight"]]):
+        w[len(bands)] = log(v + 1e-10)
+    bands.append(bandNumber)
+
+sampleBands = [40, 54, 99, 110, 131, 155]
+
+# view(Scatter(x=bands, y=water[:len(bands)], marker=None, connector="unsorted"))
+view(Overlay(Grid(vert=map(wavelength, sampleBands)), Scatter(x=map(wavelength, bands), y=water[:len(bands)], marker=None, connector="unsorted"), Legend([[Style(linecolor="black"), "water"], [Grid(), "6 sample bands"]], colwid=[0.3, 0.7], justify="cl", width=0.45), xmin=450., xmax=2400., xlabel="wavelength [nm]", ylabel="log(radiance) [log(W/m/m/sr/micron)]"), fileName="plot_water_samplebands.svg")
+
+# view(Overlay(*([Grid(vert=map(wavelength, sampleBands))] + [Scatter(x=map(wavelength, bands), y=(w[:len(bands)] - w[:len(bands)].mean()), marker=None, connector="unsorted", linecolor=c) for w, c in zip(random.sample(waters, 20), darkseries(20))]), xmin=450., xmax=2400.))
+
+# for w in wakes:
+#     view(Scatter(x=bands, y=(w[:len(bands)] - water[:len(bands)]), marker=None, connector="unsorted", ymin=-3., ymax=3.))
+#     time.sleep(0.1)
+
+# view(Overlay(*[Scatter(x=map(wavelength, bands), y=(w[:len(bands)] - water[:len(bands)] - w[:len(bands)].mean()), marker=None, connector="unsorted", linecolor=c) for w, c in zip(random.sample(wakes, 20), darkseries(20))]))
+
+view(Overlay(*([Grid(vert=map(wavelength, sampleBands))] + [Scatter(x=map(wavelength, bands), y=(w[:len(bands)] - water[:len(bands)] - w[:len(bands)].mean()), marker=None, connector="unsorted", linecolor=c) for w, c in zip(random.sample(wakes, 20), darkseries(20))] + [Legend([[Style(linecolor="red"), "wake/water"], [Grid(), "6 sample bands"]], colwid=[0.3, 0.7], justify="cl", width=0.45)]), xmin=450., xmax=2400., ymax=3.), xlabel="wavelength [nm]", ylabel="log(normalized wake radiance / water radiance)", fileName="plot_wake_samplebands.png")
+
+# for w in clouds:
+#     view(Scatter(x=bands, y=(w[:len(bands)] - water[:len(bands)]), marker=None, connector="unsorted", ymin=-3., ymax=5.))
+#     time.sleep(0.1)
+
+# view(Overlay(*([Grid(vert=map(wavelength, sampleBands))] + [Scatter(x=map(wavelength, bands), y=(w[:len(bands)] - water[:len(bands)] - w[:len(bands)].mean()), marker=None, connector="unsorted", linecolor=c) for w, c in zip(random.sample(clouds, 20), darkseries(20))]), xmin=450., xmax=2400.))
+
+view(Overlay(*([Grid(vert=map(wavelength, sampleBands))] + [Scatter(x=map(wavelength, bands), y=(w[:len(bands)] - water[:len(bands)] - w[:len(bands)].mean()), marker=None, connector="unsorted", linecolor=c) for w, c in zip(random.sample(clouds, 20), darkseries(20))] + [Legend([[Style(linecolor="red"), "cloud/water"], [Grid(), "6 sample bands"]], colwid=[0.3, 0.7], justify="cl", width=0.45)]), xmin=450., xmax=2400., ymax=3.5), xlabel="wavelength [nm]", ylabel="log(normalized cloud radiance / water radiance)", fileName="plot_cloud_samplebands.png")
+
+view(Overlay(*([Grid(vert=map(wavelength, sampleBands))] + [Scatter(x=map(wavelength, bands), y=((w1[:len(bands)] - w1[:len(bands)].mean()) - (w2[:len(bands)] - w2[:len(bands)].mean())), marker=None, connector="unsorted", linecolor=c) for w1, w2, c in zip(random.sample(wakes, 20), random.sample(clouds, 20), darkseries(20))] + [Legend([[Style(linecolor="red"), "wake/cloud"], [Grid(), "6 sample bands"]], colwid=[0.3, 0.7], justify="cl", width=0.45)]), xmin=450., xmax=2400.), xlabel="wavelength [nm]", ylabel="log(normalized wake radiance / normalized cloud radiance)", fileName="plot_wakes_clouds_samplebands.png")
+
+#####################################################################################
+
+images = {}
+for band in sampleBands:
+    images[band] = gdal.Open(glob.glob(inputDir + "/*_B%03d_L1T.TIF" % band)[0], gdalconst.GA_ReadOnly)
+    if band < 70.5:
+        images[band] = images[band].GetRasterBand(1).ReadAsArray()[2200:2800,197:358] / 40.
+    else:
+        images[band] = images[band].GetRasterBand(1).ReadAsArray()[2200:2800,197:358] / 80.
+
+plots = {}
+for i in xrange(len(sampleBands)):
+    for j in xrange(i+1, len(sampleBands)):
+        watery = zip(images[sampleBands[i]][masks["pure-water"]], images[sampleBands[j]][masks["pure-water"]])
+        wake = zip(images[sampleBands[i]][masks["wake-tight"]], images[sampleBands[j]][masks["wake-tight"]])
+        clouds = zip(images[sampleBands[i]][masks["clouds-tight"]], images[sampleBands[j]][masks["clouds-tight"]])
+
+        waterplot = Scatter(watery, sig=["x", "y"], limit=1000, markercolor="blue")
+        wakeplot = Scatter(wake, sig=["x", "y"], limit=1000, markercolor="black")
+        cloudsplot = Scatter(clouds, sig=["x", "y"], limit=1000, markercolor="red")
+
+        plots[i,j] = Overlay(waterplot, wakeplot, cloudsplot, xlabel="band %d" % sampleBands[i], ylabel="band %d" % sampleBands[j], bottommargin=0.2, xlabeloffset=0.2, leftmargin=0.2, ylabeloffset=-0.15)
+
+view(Layout(5, 5,
+            None,       None,       None,       None,       plots[0,1],
+            None,       None,       None,       plots[1,2], plots[0,2],
+            None,       None,       plots[2,3], plots[1,3], plots[0,3],
+            None,       plots[3,4], plots[2,4], plots[1,4], plots[0,4],
+            plots[4,5], plots[3,5], plots[2,5], plots[1,5], plots[0,5]), width=2500, height=2500, fileName="plot_6x6grid.svg")
+
+
