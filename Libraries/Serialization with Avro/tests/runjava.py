d63ec2058c55592e6f1e35ee8377c273888580bf
Jim Pivarski <pivarski@jim-ubuntu1204.(none)>
2012-08-21 10:47:00 -0500

    Test with a new version of Java

--- Libraries/Serialization with Avro/tests/runjava.py
+++ Libraries/Serialization with Avro/tests/runjava.py
@@ -1,16 +1,16 @@
 import jpype
 import struct
 
-classpath = "/home/pivarski/odg/matsu-project/Libraries/Serialization with Avro/geoPictureSerializer.jar"
-# jvmpath = jpype.getDefaultJVMPath()
-jvmpath = "/usr/lib/jvm/java-6-openjdk/jre/lib/amd64/server/libjvm.so"
+classpath = "/home/pivarski/matsu-project/Libraries/Serialization with Avro/geoPictureSerializer.jar"
+jvmpath = jpype.getDefaultJVMPath()
+# jvmpath = "/usr/lib/jvm/java-6-openjdk/jre/lib/amd64/server/libjvm.so"
 
 jpype.startJVM(jvmpath, "-Djava.class.path=%s" % classpath)
 GeoPictureSerializer = jpype.JClass("org.occ.matsu.GeoPictureSerializer")
 
 geoPictureSerializer = GeoPictureSerializer()
 
-geoPictureSerializer.loadSerialized(open("/home/pivarski/NOBACKUP/GobiDesert01-small.serialized").read())
+geoPictureSerializer.loadSerialized(open("/home/pivarski/NOBACKUP/matsu_serialized/GobiDesert01.serialized").read())
 
 print geoPictureSerializer.image("B016", "B023", "B029", 0., 0., True)
 
