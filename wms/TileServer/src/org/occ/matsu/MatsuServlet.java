5bab4a15d036583009c5620d31836b67fc89aac2
Jim Pivarski <pivarski@jim-ubuntu1204.(none)>
2012-08-27 18:41:23 -0500

    Insert CONFIG.ini options at compile-time for TileServer

--- wms/TileServer/src/org/occ/matsu/MatsuServlet.java
+++ wms/TileServer/src/org/occ/matsu/MatsuServlet.java
@@ -24,12 +24,12 @@ import org.apache.accumulo.core.client.AccumuloException;
 import org.apache.accumulo.core.client.AccumuloSecurityException;
 import org.apache.accumulo.core.client.TableNotFoundException;
 
-public class MatsuServlet extends HttpServlet {
-    protected static String accumuloName = "accumulo";
-    protected static String zooKeeperList = "192.168.18.101:2181";
-    protected static String userId = "root";
-    protected static String password = "password";
-    protected static String tableName = "MatsuLevel2Tiles";
+public class MatsuServlet { // extends HttpServlet {
+    protected static String accumuloName = @ACCUMULO_DB_NAME@;
+    protected static String zooKeeperList = @ACCUMULO_ZOOKEEPER_LIST@;
+    protected static String userId = @ACCUMULO_USER_NAME@;
+    protected static String password = @ACCUMULO_PASSWORD@;
+    protected static String tableName = @ACCUMULO_TABLE_NAME@;
 
     protected String configPath;
 
@@ -37,65 +37,65 @@ public class MatsuServlet extends HttpServlet {
     protected static Connector connector = null;
 
     static {
-	zooKeeperInstance = new ZooKeeperInstance(accumuloName, zooKeeperList);
+    	zooKeeperInstance = new ZooKeeperInstance(accumuloName, zooKeeperList);
 
-	try {
-	    connector = zooKeeperInstance.getConnector(userId, password.getBytes());
-	}
-	catch (AccumuloException exception) {
-	    connector = null;
-	}
-	catch (AccumuloSecurityException exception) {
-	    connector = null;
-	}
+    	try {
+    	    connector = zooKeeperInstance.getConnector(userId, password.getBytes());
+    	}
+    	catch (AccumuloException exception) {
+    	    connector = null;
+    	}
+    	catch (AccumuloSecurityException exception) {
+    	    connector = null;
+    	}
     }
 
     @Override public void init(ServletConfig servletConfig) throws ServletException {
-	super.init(servletConfig);
+    	super.init(servletConfig);
 
-	ServletContext servletContext = servletConfig.getServletContext();
-	configPath = servletContext.getRealPath(servletConfig.getInitParameter("config.path"));
+    	ServletContext servletContext = servletConfig.getServletContext();
+    	configPath = servletContext.getRealPath(servletConfig.getInitParameter("config.path"));
     }
 
     @Override protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-	processRequest(request, response);
+    	processRequest(request, response);
     }
 
     @Override protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-	processRequest(request, response);
+    	processRequest(request, response);
     }
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
-	if (zooKeeperInstance == null  ||  connector == null) { return; }
+    	if (zooKeeperInstance == null  ||  connector == null) { return; }
 
-	Scanner scanner;
-	try {
-	    scanner = connector.createScanner(tableName, Constants.NO_AUTHS);
-	}
-	catch (TableNotFoundException exception) {
-	    return;
-	}
+    	Scanner scanner;
+    	try {
+    	    scanner = connector.createScanner(tableName, Constants.NO_AUTHS);
+    	}
+    	catch (TableNotFoundException exception) {
+    	    return;
+    	}
 
-	String key = request.getParameter("key");
-	if (key == null) { return; }
+    	String key = request.getParameter("key");
+    	if (key == null) { return; }
 
-	scanner.setRange(new Range(key));
+    	scanner.setRange(new Range(key));
 
-	for (Entry<Key, Value> entry : scanner) {
-	    String columnName = entry.getKey().getColumnQualifier().toString();
-	    if (columnName.equals("l2png")) {
-		byte[] l2png = entry.getValue().get();
+    	for (Entry<Key, Value> entry : scanner) {
+    	    String columnName = entry.getKey().getColumnQualifier().toString();
+    	    if (columnName.equals("l2png")) {
+    		byte[] l2png = entry.getValue().get();
 
-		response.setContentType("image/png");
-		response.setContentLength(l2png.length);
+    		response.setContentType("image/png");
+    		response.setContentLength(l2png.length);
 
-		ServletOutputStream servletOutputStream = response.getOutputStream();
-		servletOutputStream.write(l2png);
-		servletOutputStream.close();
+    		ServletOutputStream servletOutputStream = response.getOutputStream();
+    		servletOutputStream.write(l2png);
+    		servletOutputStream.close();
 
-		break;
-	    }
-	}
+    		break;
+    	    }
+    	}
     }
 
 }
